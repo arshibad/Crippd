1c0803abbde92b6f3a5d7f82b298d9ca15952464
Jignesh <nirav.changela@phpprogrammers.com>
2013-12-28 13:42:57 +0530

    check device

---
+++ js/index.js
@@ -0,0 +1,27 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function() {
+		setTimeout(function(){
+        window.open('http://crippd.com', '_self', 'location=yes'); 
+      },5000);
+      
+    }
+};
